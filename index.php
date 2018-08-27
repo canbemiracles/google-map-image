78a6f3fbb5a639f5f0aeca545658583ba8fdddca
canbemiracles <canbe.miracles@gmail.com>
2018-08-27 18:56:19 +0800

    size value

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 function gen_map_img($x, $y) {
-  $image = file_get_contents('http://maps.googleapis.com/maps/api/staticmap?center='. $x .',' . $y . '&zoom=8&size=150x100&markers=color%3ablue%7Clabel%3aS%7C11211&sensor=false'); 
+
+  $width = 300;
+  $height = 300;
+  $image = file_get_contents('http://maps.googleapis.com/maps/api/staticmap?center='. $x .',' . $y . '&zoom=8&size=' . $width . 'x'. $height .'&markers=color%3ablue%7Clabel%3aS%7C11211&sensor=false'); 
   $fp  = fopen('sample.png', 'w+'); 
 
   fputs($fp, $image); 
